9053c165de66a152e0266d5093174e88b38c8507
dipto0321 <diptokmk47@gmail.com>
2018-10-31 01:20:40 +0600

    Add view files for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
+  def index
+    # do code
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      redirect_to 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :tags, :content)
+  end
 end
