e634b50f6de46f7336c1b51bcfac07043b966907
dipto0321 <diptokmk47@gmail.com>
2018-10-31 23:05:43 +0600

    Fix some rubocop style

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :find_post, only: [:show, :update, :edit, :destroy]
+  before_action :find_post, only: %i[show update edit destroy]
   def index
     @posts = Post.all.order('created_at DESC')
   end
